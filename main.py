5f68dd1884201578a6d6952a5bd6025bd2a486cd
gmberton <s252805@studenti.polito.it>
2024-04-09 04:50:54 +0200

    .

--- main.py
+++ main.py
@@ -155,6 +155,7 @@ for query_name, results in results_per_query.items():
                 located_cldp_l_40 += 1
             break
 
+logging.info(f"{threshold=}")
 logging.info(
     f"{located=} "
     f"{located_fclt_le_200=} {located_fclt_200_400=} {located_fclt_400_800=} {located_fclt_g_800=} "
