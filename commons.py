9e65f5ce0a3dd8ebdda99d714740eef9a2b875fd
Gabriele Berton <s252805@studenti.polito.it>
2024-04-12 23:55:34 -0400

    .

--- commons.py
+++ commons.py
@@ -1,8 +1,27 @@
 
 import sys
+import torch
+import random
 import logging
 import traceback
+import numpy as np
 from pathlib import Path
+
+
+def make_deterministic(seed: int = 0):
+    """Make results deterministic. If seed == -1, do not make deterministic.
+        Running your script in a deterministic way might slow it down.
+        Note that for some packages (eg: sklearn's PCA) this function is not enough.
+    """
+    seed = int(seed)
+    if seed == -1:
+        return
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def setup_logging(log_dir: Path, stdout: str = "debug", info_filename: str = "info.log",
